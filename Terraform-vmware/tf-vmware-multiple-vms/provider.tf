5d5aeafb8e5a51d71103360bed09799d65781707
lmendesb <lmendesb@cisco.com>
2022-06-08 16:37:40 -0300

    Add files

---
+++ Terraform-vmware/tf-vmware-multiple-vms/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.1.1"
+    }
+  }
+}
+
+provider "vsphere" {
+  user           = var.vcenter_username
+  password       = var.vcenter_password
+  vsphere_server = var.vcenter_server
+  allow_unverified_ssl = true
+}
